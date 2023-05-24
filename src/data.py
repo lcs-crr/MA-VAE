337e6f5bf471fd0a53a83160fdb582d74ecc52cb
lcs-crr <lfc.portus@gmail.com>
2023-05-24 17:51:16 +0200

    Updated data script

--- src/data.py
+++ src/data.py
@@ -25,8 +25,8 @@ freq_target = 2
 filter_order = 1
 
 # Specify sampling frequencies of the chosen channels
-sampling_freqs = [freq_A, freq_B, freq_C, freq_D, freq_E, freq_F, freq_G, freq_H, freq_I,
-                  freq_J, freq_K, freq_L, freq_M, freq_N, freq_O, freq_P, freq_Q, freq_R]
+sampling_freqs = [freq_1, freq_2, freq_3, freq_4, freq_5, freq_6, freq_7,
+                  freq_8, freq_9, freq_10, freq_11, freq_12, freq_13]
 
 
 def butter_lowpass_filter(data, cutoff_freq, order):
@@ -49,27 +49,24 @@ def mf4_parser(path):
         mdf = mdf.resample(1/max(sampling_freqs))
 
         # Obtain desired signals from mdf object
-        A = mdf.get('A')
-        B = mdf.get('B')
-        C = mdf.get('C')
-        D = mdf.get('D')
-        E = mdf.get('E')
-        F = mdf.get('F')
-        G = mdf.get('G')
-        H = mdf.get('H')
-        I = mdf.get('I')
-        J = mdf.get('J')
-        K = mdf.get('K')
-        L = mdf.get('L')
-        M = mdf.get('M')
-        N = mdf.get('N')
-        O = mdf.get('O')
-        P = mdf.get('P')
-        Q = mdf.get('Q')
-        R = mdf.get('R')
+        channel1 = mdf.get('Channel1')
+        channel2 = mdf.get('Channel2')
+        channel3 = mdf.get('Channel3')
+        channel4 = mdf.get('Channel4')
+        channel5 = mdf.get('Channel5')
+        channel6 = mdf.get('Channel6')
+        channel7 = mdf.get('Channel7')
+        channel8 = mdf.get('Channel8')
+        channel9 = mdf.get('Channel9')
+        channel10 = mdf.get('Channel10')
+        channel11 = mdf.get('Channel11')
+        channel12 = mdf.get('Channel12')
+        channel13 = mdf.get('Channel13')
+
 
         # Create list of signals to iterate later
-        mf4_channel_list = [A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R]
+        mf4_channel_list = [channel1, channel2, channel3, channel4, channel5, channel6, channel7,
+                            channel8, channel9, channel10, channel11, channel12, channel13]
 
         temp = []
         for idx, channel in enumerate(mf4_channel_list):
@@ -121,153 +118,177 @@ def mf4_parser(path):
 
 # Parse normal data
 normal_data = mf4_parser('NORMAL_LOAD_PATH')
-# -> List of 1875 multivariate time-series of varying length
+# -> List of multivariate time-series of varying length
 
 # Parse anomalous data
 anomalous_data = mf4_parser('ANOMALOUS_LOAD_PATH')
-# -> List of 60 multivariate time-series of varying length
+# -> List of multivariate time-series of varying length
 
-# Shuffle normal data
-random.shuffle(normal_data)
-
-# Split normal_data into training and test data
-train_list, test_list = sklearn.model_selection.train_test_split(
-    normal_data, random_state=seed, test_size=0.3)
-# -> List of 1312, 563 multivariate time-series of varying length
-
-# Split training data into training and validation subsets
-train_list, val_list = sklearn.model_selection.train_test_split(
-    train_list, random_state=seed, test_size=0.2)
-# -> List of 1049, 263 multivariate time-series of varying length
+meas_time = []
+for sequence in normal_data:
+    meas_time.append(len(sequence) / (2*60))  # 2*60 converts samples to minutes
+# Find total dynamic testing time
+cumsum = np.cumsum(meas_time)
 
 
-## Process training and validation data
-# Find statistical metrics, such as mean and standard deviation
-def find_stat_metrics(data):
-    mean = np.mean(np.vstack(data), axis=0)
-    std_dev = np.std(np.vstack(data), axis=0)
-    return [mean, std_dev]
+def find_nearest(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx], idx
 
 
-stat_metrics = find_stat_metrics(train_list)
-# -> List of metrics
+# Find index of sequence that comes closest to 1h of testing time
+_, idx_1 = find_nearest(cumsum, 1)
+# Find index of sequence that comes closest to 8h of testing time
+_, idx_8 = find_nearest(cumsum, 8)
+# Find index of sequence that comes closest to 64h of testing time
+_, idx_64 = find_nearest(cumsum, 64)
+# Find index of sequence that comes closest to 512h of testing time
+_, idx_512 = find_nearest(cumsum, 512)
+
+train_list_1h = normal_data[:idx_1]
+train_list_8h = normal_data[:idx_8]
+train_list_64h = normal_data[:idx_64]
+train_list_512h = normal_data[:idx_512]
+train_list_list = [train_list_1h, train_list_8h, train_list_64h, train_list_512h]
+
+test_list = normal_data[idx_512:]
+
+versions = ['1h', '8h', '64h', '512h']
+
+for i, train_list in enumerate(train_list_list):
+    train_list, val_list = sklearn.model_selection.train_test_split(
+        train_list, random_state=seed, test_size=0.2)
+    # -> Two lists multivariate time-series of varying length
+
+    ## Process training and validation data
+    # Find statistical metrics, such as mean and standard deviation
+    def find_stat_metrics(data):
+        mean = np.mean(np.vstack(data), axis=0)
+        std_dev = np.std(np.vstack(data), axis=0)
+        return [mean, std_dev]
 
 
-def standardise(data, stat_metrics):
-    mean = stat_metrics[0]
-    std_dev = stat_metrics[1]
-    data_scaled = []
-    for time_series in data:
-        data_scaled.append((time_series - mean) / std_dev)
-    return data_scaled
+    stat_metrics = find_stat_metrics(train_list)
+    # -> List of metrics
 
 
-# Standardise data, i.e. transform it such that mean=0 and std_dev=1
-scaled_train_list = list(standardise(train_list, stat_metrics))
-# -> List of 1049 multivariate time-series of varying length
-scaled_val_list = list(standardise(val_list, stat_metrics))
-# -> List of 263 multivariate time-series of varying length
+    def standardise(data, stat_metrics):
+        mean = stat_metrics[0]
+        std_dev = stat_metrics[1]
+        data_scaled = []
+        for time_series in data:
+            data_scaled.append((time_series - mean) / std_dev)
+        return data_scaled
 
 
-def window_list(data, window_size, window_shift):
-    window_list_temp = []
-    # For time series in data
-    for time_series in data:
-        # Find number of resulting windows given the window size and shift
-        set_window_count = int((time_series.shape[0] - window_size) / window_shift + 1)
-        # Pre-allocate output array
-        window_data = np.zeros((set_window_count, window_size, time_series.shape[1]))
-        # For number of resulting windows
-        for window in range(set_window_count):
-            # For features in time series
-            for feature in range(time_series.shape[1]):
-                window_data[window, :, feature] = \
-                    time_series[window * window_shift:window_size + window * window_shift, feature]
-        window_list_temp.append(window_data)
-    # Concatenate list into 3D array of shape (number of windows, window size, features)
-    windows = np.concatenate(window_list_temp[:], axis=0)
-    return windows
+    # Standardise data, i.e. transform it such that mean=0 and std_dev=1
+    scaled_train_list = list(standardise(train_list, stat_metrics))
+    # -> List multivariate time-series of varying length
+    scaled_val_list = list(standardise(val_list, stat_metrics))
+    # -> List multivariate time-series of varying length
 
 
-# Specify window size, shift and features
-window_size = 256
-window_shift = 128
-features = 13
+    def window_list(data, window_size, window_shift):
+        window_list_temp = []
+        # For time series in data
+        for time_series in data:
+            # Find number of resulting windows given the window size and shift
+            set_window_count = int((time_series.shape[0] - window_size) / window_shift + 1)
+            # Pre-allocate output array
+            window_data = np.zeros((set_window_count, window_size, time_series.shape[1]))
+            # For number of resulting windows
+            for window in range(set_window_count):
+                # For features in time series
+                for feature in range(time_series.shape[1]):
+                    window_data[window, :, feature] = \
+                        time_series[window * window_shift:window_size + window * window_shift, feature]
+            window_list_temp.append(window_data)
+        # Concatenate list into 3D array of shape (number of windows, window size, features)
+        windows = np.concatenate(window_list_temp[:], axis=0)
+        return windows
 
-# Window training and validation data with given window size and shift
-scaled_train_window = window_list(scaled_train_list, window_size, window_shift)
-# -> 3D array of shape (4937, window size, features)
-scaled_val_window = window_list(scaled_val_list, window_size, window_shift)
-# -> 3D array of shape (1216, window size, features)
 
+    # Specify window size, shift and features
+    window_size = 256
+    window_shift = 128
+    features = 13
 
-def generator(data):
-    for i in range(len(data)):
-        yield data[i, :, :], data[i, :, :]
-
-
-# Create tf.data objects
-tf_train = tf.data.Dataset.from_generator(lambda: generator(scaled_train_window),
-                                          output_types=(tf.as_dtype('float32'),
-                                                        tf.as_dtype('float32')),
-                                          output_shapes=(tf.TensorShape([window_size, features]),
-                                                         tf.TensorShape([window_size, features])))
-
-tf_val = tf.data.Dataset.from_generator(lambda: generator(scaled_val_window),
-                                        output_types=(tf.as_dtype('float32'),
-                                                      tf.as_dtype('float32')),
-                                        output_shapes=(tf.TensorShape([window_size, features]),
-                                                       tf.TensorShape([window_size, features])))
-
-# Find number of windows in each array to enable shuffling later
-n_train_data = len(scaled_train_window)
-n_val_data = len(scaled_val_window)
-
-# Define desired batch size
-batch_size = 256
-
-# Shuffle and batch data
-tf_train = tf_train.shuffle(n_train_data).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-tf_val = tf_val.shuffle(n_val_data).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-
-# Save training and validation data
-tf.data.experimental.save(tf_train, 'SAVE_PATH')
-tf.data.experimental.save(tf_val, 'SAVE_PATH')
-
-## Process test data
-# Standardise normal test data, i.e. transform it such that mean=0 and std_dev=1
-scaled_list = list(standardise(test_list, stat_metrics))
-# Pre-allocate list where each item represents the windowed time series
-scaled_windowed_list = []
-# For time series in test list
-for time_series in scaled_list:
     # Window training and validation data with given window size and shift
-    windowed_time_series_temp = window_list(list(time_series), window_size, window_size)
+    scaled_train_window = window_list(scaled_train_list, window_size, window_shift)
     # -> 3D array of shape (number of windows, window size, features)
-    scaled_windowed_list.append(windowed_time_series_temp)
-    # -> List of 3D arrays, each shape (number of windows, window size, features)
-# Concatenate all list items into one array
-scaled_normal_test_window = np.vstack(scaled_windowed_list)
-# -> 3D array of shape (2606, window size, features)
-
-# Standardise anomalous test data, i.e. transform it such that mean=0 and std_dev=1
-scaled_list = list(standardise(anomalous_data, stat_metrics))
-# Pre-allocate list where each item represents the windowed time series
-scaled_windowed_list = []
-# For time series in test list
-for time_series in scaled_list:
-    # Window training and validation data with given window size and shift
-    windowed_time_series_temp = window_list(list(time_series), window_size, window_size)
+    scaled_val_window = window_list(scaled_val_list, window_size, window_shift)
     # -> 3D array of shape (number of windows, window size, features)
-    scaled_windowed_list.append(windowed_time_series_temp)
-    # -> List of 3D arrays, each shape (number of windows, window size, features)
-# Concatenate all list items into one array
-scaled_anomalous_test_window = np.vstack(scaled_windowed_list)
-# -> 3D array of shape (225, window size, features)
 
-# Save normal and anomalous test data as pickle files
-with open('SAVE_PATH', 'wb') as f:
-    pickle.dump(scaled_normal_test_window, f)
 
-with open('SAVE_PATH', 'wb') as f:
-    pickle.dump(scaled_anomalous_test_window, f)
+    def generator(data):
+        for i in range(len(data)):
+            yield data[i, :, :], data[i, :, :]
+
+
+    # Create tf.data objects
+    tf_train = tf.data.Dataset.from_generator(lambda: generator(scaled_train_window),
+                                              output_types=(tf.as_dtype('float32'),
+                                                            tf.as_dtype('float32')),
+                                              output_shapes=(tf.TensorShape([window_size, features]),
+                                                             tf.TensorShape([window_size, features])))
+
+    tf_val = tf.data.Dataset.from_generator(lambda: generator(scaled_val_window),
+                                            output_types=(tf.as_dtype('float32'),
+                                                          tf.as_dtype('float32')),
+                                            output_shapes=(tf.TensorShape([window_size, features]),
+                                                           tf.TensorShape([window_size, features])))
+
+    # Find number of windows in each array to enable shuffling later
+    n_train_data = len(scaled_train_window)
+    n_val_data = len(scaled_val_window)
+
+    # Define desired batch size
+    batch_size = 512
+
+    # Shuffle and batch data
+    tf_train = tf_train.shuffle(n_train_data).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    tf_val = tf_val.shuffle(n_val_data).batch(batch_size).prefetch(tf.data.AUTOTUNE)
+
+    # Save training and validation data
+    tf.data.experimental.save(tf_train, 'SAVE_PATH')
+    tf.data.experimental.save(tf_val, 'SAVE_PATH')
+
+    ## Process test data
+    window_shift = 1
+    # Standardise normal test data, i.e. transform it such that mean=0 and std_dev=1
+    scaled_list = list(standardise(test_list, stat_metrics))
+    # Pre-allocate list where each item represents the windowed time series
+    scaled_windowed_list = []
+    # For time series in test list
+    for time_series in scaled_list:
+        # Window training and validation data with given window size and shift
+        windowed_time_series_temp = window_list(list(time_series), window_size, window_shift)
+        # -> 3D array of shape (number of windows, window size, features)
+        scaled_windowed_list.append(windowed_time_series_temp)
+        # -> List of 3D arrays, each shape (number of windows, window size, features)
+    # Concatenate all list items into one array
+    scaled_normal_test_window = np.vstack(scaled_windowed_list)
+    # -> 3D array of shape (number of windows, window size, features)
+
+    # Standardise anomalous test data, i.e. transform it such that mean=0 and std_dev=1
+    scaled_list = list(standardise(anomalous_data, stat_metrics))
+    # Pre-allocate list where each item represents the windowed time series
+    scaled_windowed_list = []
+    # For time series in test list
+    for time_series in scaled_list:
+        # Window training and validation data with given window size and shift
+        windowed_time_series_temp = window_list(list(time_series), window_size, window_shift)
+        # -> 3D array of shape (number of windows, window size, features)
+        scaled_windowed_list.append(windowed_time_series_temp)
+        # -> List of 3D arrays, each shape (number of windows, window size, features)
+    # Concatenate all list items into one array
+    scaled_anomalous_test_window = np.vstack(scaled_windowed_list)
+    # -> 3D array of shape (number of windows, window size, features)
+
+    # Save normal and anomalous test data as pickle files
+    with open(os.path.join('SAVE_PATH', versions[i], 'FILE_NAME', 'wb')) as f:
+        pickle.dump(scaled_normal_test_window, f)
+
+    with open(os.path.join('SAVE_PATH', versions[i], 'FILE_NAME', 'wb')) as f:
+        pickle.dump(scaled_anomalous_test_window, f)
